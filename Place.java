b0c3fa0929bf641b05c7ec21219d6c14effcfe26
kailyn0215 <kailymbrown15@gmail.com>
2025-09-10 12:46:29 -0400

    !!

--- Place.java
+++ Place.java
@@ -1,3 +1,10 @@
+/** Creates a Place with the given zip, town name, and
+* state
+* @param zip The 5-digit zip code
+* @param town The town name
+* @param state The state abbreviation
+*/
+
 public class Place {
     
 }
