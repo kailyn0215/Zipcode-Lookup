1686111353f8fa6d49c0e0d43401968b3aeb3bc4
kailyn0215 <brownkai24@7hills.org>
2025-09-11 15:51:32 -0400

    fixed Place

--- Place.java
+++ Place.java
@@ -1,32 +1,33 @@
-/** Creates a Place with the given zip, town name, and
-* state
-* @param zip The 5-digit zip code
-* @param town The town name
-* @param state The state abbreviation
-*/
+public class Place {
+/** Creates a Place with the given zip, town name, and state
+    * @param zip The 5-digit zip code
+    * @param town The town name
+    * @param state The state abbreviation
+    */
 
-String z;
-String t;
-String s;
+    private String zip;
+    private String town;
+    private String state;
 
-public Place(String zip, String town, String state) {
-    z = zip;
-    t = town;
-    s = state;
-}
+    public Place(String zip, String town, String state) {
+        this.zip = zip;
+        this.town = town;
+        this.state = state;
+    }
 
-public String toString() {
-    return t + ", " + s;
-}
+    public String toString() {
+        return town + ", " + state;
+    }
 
-public String getZip() {
-    return z;
-}
+    public String getZip() {
+        return zip;
+    }
 
-public String getTown() {
-    return t;
-}
+    public String getTown() {
+        return town;
+    }
 
-public String getState() {
-    return s;
+    public String getState() {
+        return state;
+    }
 }
